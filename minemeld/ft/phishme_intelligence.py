98aa11fbba4d108a6d52fff81fddaed53c220da3
Josh Larkins <josh.larkins@phishme.com>
2016-08-17 13:23:36 -0400

    stash

---
+++ minemeld/ft/phishme_intelligence.py
@@ -0,0 +1,32 @@
+"""
+
+"""
+
+import logging
+import requests
+
+from . import basepoller
+
+LOG = logging.getLogger(__name__)
+
+
+class PhishMeIntelligence(basepoller.BasePollerFT):
+    """
+
+    """
+    def configure(self):
+        pass
+
+    def _process_item(self, item):
+        """
+
+        :param item:
+        :return:
+        """
+
+    def _build_iterator(self, now):
+        """
+
+        :param now:
+        :return:
+        """
